e46e114290fcfac32a2b5d007cc2f5115a6ac59c
rethes <rethesmutua@gmail.com>
2026-08-08 20:34:00 +0300

    fix(authRoutes) Fix route paths for register and login endpoints

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,6 +4,6 @@ import { register, login } from "../controllers/authController.js";
 const router = Router();
 
 router.post("/registerpage", register);
-router.post("/loginpage", login);
+router.post("/loginpages", login);
 
 export default router;
