09e271aae1f5450a4354479bdd6862eeb9d92a7c
rethes <rethesmutua@gmail.com>
2026-08-08 20:14:12 +0300

    fix(authRoutes): Update auth routes to match frontend endpoints
    Closes #8

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { register, login } from "../controllers/authController.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/registerpage", register);
+router.post("/loginpage", login);
 
 export default router;
