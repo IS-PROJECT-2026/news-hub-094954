ea3ce824b108b67f2de5d32df77ae7010b4505f5
rethes <rethesmutua@gmail.com>
2026-08-08 20:14:12 +0300

    Modified line on feat/8-user-registration-login-endpoint branch

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,6 +4,6 @@ import { register, login } from "../controllers/authController.js";
 const router = Router();
 
 router.post("/register", register);
-router.post("/login", login);
+router.post("/loginpage", login);
 
 export default router;
